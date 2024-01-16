4affc787624892f689f52fa6c4c9254b5f349cc5
Chibadoii <ghostrider.max@mail.ru>
2024-01-17 00:57:51 +0300

    no way

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 
+
 fn main() {
     let file_path: String = String :: from ("text.txt");
     // Считываем файл
@@ -8,32 +9,33 @@ fn main() {
 
     //Разделяем и заносим в vec 
     let collection = contents.split("\n").collect::<Vec<&str>>();
+    // вектор вариантов подстановки
+    let replacements =vec![
+        ("one","1"),("two","2"),("three","3"),("four","4"),("five","5"), ("six","6"),("seven","7"),("eight", "9"), ("zero","0")
+    ];
+    //println!("{:?}", collection);
+    //let mut final_num = vec![];
+    let mut words_in_string = vec![];// хранение слов из втроки
+    //for item in &collection{
+         for (word,digit) in &replacements  {
+              if collection[0].contains(word) || collection[0].contains(digit) {
 
-    // инициализация числового и строкового массива
-    let num_arr = ["1","2","3","4","5","6","7","8","9","0"]; 
-    let str_arr = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    
-    let mut vremennaya = String::new();//Временная переменная для хранения символов в Стринге
-    for i in 0..collection.len(){
-        let mut var = collection[i].trim().to_string();//первая строка без пробелов
-        let char_collection = var.split("").collect::<Vec<&str>>();//Посимвольно разделенная строка в векторе
-        for c in 0..char_collection.len(){
-            let mut chr = char_collection[c].to_string();//отдельный символ строки
-                if chr.contains(num_arr[c]){
-                    vremennaya = num_arr[c].to_string();
-                    println!("Занесена первая цифровая часть");
+                        words_in_string.push(digit); // Добавляем стринг в вектор
+                        println!("Число успешно добавлен в вектор");
+                    }
                 }
-                else if chr.contains(str_arr[c]){
-                    vremennaya = str_arr[c].to_string();
-                    println!("Занесена первая буквенная часть");
-                }else {
-                    let b = c + 1;
-                    chr.push_str(char_collection[b]);
-                    println!("Добавлен симфол в тестируемую строку");
-                }
+    for i in words_in_string{
+        println!("{}", i);
+    }
+
+            /*final_num.push(words_in_string.get(words_in_string[0]));
+            final_num.push(words_in_string.get(words_in_string.len() - 1));
+            println!("{:?}", final_num)*/
+            /*final_num.push(words_in_string[0]);
+            final_num.push(words_in_string.len() - 1);*/
 
             }
-        }
-        println!("{}", vremennaya);
 
-    }
+
+
+
