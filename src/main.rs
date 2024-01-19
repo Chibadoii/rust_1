8d1f6ee2d29160d0ddddf326d8a61a6485092478
Chibadoii <ghostrider.max@mail.ru>
2024-01-19 04:29:10 +0300

    Другой подход, ошибки типов

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,40 @@ use std::fs;
 
 
 fn main() {
-    let file_path: String = String :: from ("text.txt");
+    let file_path: String = String::from("text.txt");
     // Считываем файл
     let contents = fs::read_to_string(file_path)
         .expect("Err");
 
     //Разделяем и заносим в vec 
     let collection = contents.split("\n").collect::<Vec<&str>>();
+    //let collection = collection1.split("\r").collect::<Vec<&str>>();
     // вектор вариантов подстановки
-    let replacements =vec![
-        ("one","1"),("two","2"),("three","3"),("four","4"),("five","5"), ("six","6"),("seven","7"),("eight", "9"), ("zero","0")
+    let replacements = vec![
+        ("one", "1"), ("two", "2"), ("three", "3"), ("four", "4"), ("five", "5"), ("six", "6"), ("seven", "7"), ("eight", "9"), ("zero", "0")
     ];
-    //println!("{:?}", collection);
-    //let mut final_num = vec![];
-    let mut words_in_string = vec![];// хранение слов из втроки
-    //for item in &collection{
-         for (word,digit) in &replacements  {
-              if collection[0].contains(word) || collection[0].contains(digit) {
 
-                        words_in_string.push(digit); // Добавляем стринг в вектор
-                        println!("Число успешно добавлен в вектор");
-                    }
-                }
-    for i in words_in_string{
-        println!("{}", i);
-    }
 
-            /*final_num.push(words_in_string.get(words_in_string[0]));
-            final_num.push(words_in_string.get(words_in_string.len() - 1));
-            println!("{:?}", final_num)*/
-            /*final_num.push(words_in_string[0]);
-            final_num.push(words_in_string.len() - 1);*/
-
+    let mut couples_num = vec![];
+    for mut item1 in &collection {
+        for (word, digit) in &replacements {
+            let mut cal_str: String = item1.parse().expect("Err str in str");
+            if cal_str.contains(word) {
+                &mut cal_str.replace(word, digit);
+                //println!("{}", &item1);
             }
+            let mut combo_string = String::new();
+            let f_char = &cal_str.chars().next().expect("Err f ch");
+            let l_char = &cal_str.chars().last().expect("Err l ch");
+            combo_string = format!("{} {}", f_char, l_char);
+
+
+            //println!("{}", &combo_string.parse::<i64>().unwrap());
+            couples_num.push(combo_string).parse::<i64>().unwrap();
+        }
+    }
+    println!("{:?}", &couples_num);
+}
 
 
 
