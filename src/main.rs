7320d03787ce21a4d3682a16073588bacfee2a22
Chibadoii <ghostrider.max@mail.ru>
2024-01-24 04:21:42 +0300

    FF

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ fn main() {
 
     //Разделяем и заносим в vec 
     let collection = contents.split("\n").collect::<Vec<&str>>();
-    //let collection = collection1.split("\r").collect::<Vec<&str>>();
     // вектор вариантов подстановки
     let replacements = vec![
         ("one", "1"), ("two", "2"), ("three", "3"), ("four", "4"), ("five", "5"), ("six", "6"), ("seven", "7"), ("eight", "9"), ("zero", "0")
@@ -19,77 +18,41 @@ fn main() {
     let mut couples_num = vec![];
     for item1 in &collection {
         let mut cal_str: String = item1.parse().expect("Err str in str");
-        //cal_str = cal_str.trim().to_string();// избавляемся от пробелов
         for (word, digit) in &replacements {
-
             if cal_str.contains(word) {
-                &mut cal_str.replace(word, digit);
-                //println!("{}", &item1);
+                cal_str = cal_str.replace(word, digit).to_string().clone();//Переводим слова в числа
+                //println!("{}", &cal_str);
             }
-            //println!("{}", &combo_string.parse::<i64>().unwrap());
-            //couples_num.push(combo_string).parse::<i64>().unwrap();
-        }
-        let mut f_char = String::new();
-        for c in cal_str.chars() {
-            //let first_char:char = c;
-            if c != ' '{
-                &mut f_char.push(c);
-                break;
-            }else {
-                println!("Первый символ пустой")
-            };
-            println!("{}", c);
+
         }
 
-        let mut l_char = String::new();
-        for b in cal_str.chars().rev(){
-            if b != ' '{
-                &mut l_char.push(b);
-                break;
-            }else {
-                println!("Пробел")
-            }
-        }
-        /*loop{
-            let char_calc = ' ';
-            let mut b: usize = 0;
-            //let loc_str = &cal_str;
-            let last_char = &cal_str.chars().rev().nth(b).expect("Err last ch un str");
-            let last_char = last_char.unwrap_or_default();
-            if last_char != char_calc {
-               &mut l_char.push(last_char);
-                break;
-            }else {
-                println!("Последний символ пустой")
-            };
-            b+=1;
-            println!("{}", b);
-        }*/
+        let num_char:String = cal_str.chars().filter(|c| c.is_digit(10)).collect();//Выбираем числа
 
-        /*let f_char = &cal_str.chars().next().expect("Err f ch");
-        dbg!("{}",);
-        let l_char = &cal_str.chars().last().expect("Err l ch");*/
+        let f_char = &num_char.chars().next().expect("Err f ch");
+        //dbg!("{}", f_char);
+        let l_char = &num_char.chars().last().expect("Err l ch");
 
-        &mut combo_string.push_str(&*f_char);
-        &mut combo_string.push_str(&*l_char);
+        &mut combo_string.push(**&f_char);
+        &mut combo_string.push(**&l_char);
         //combo_string = format!("{}{}", f_char, l_char);
-        dbg!("{}", &combo_string);
+        //dbg!("{}", &combo_string);
 
 
         if let Ok(parsed_number) = combo_string.parse::<i64>() {
-            couples_num.push(parsed_number);
-            &mut combo_string.clear();
+           &mut couples_num.push(parsed_number);
+           &mut combo_string.clear();
         } else {
-            println!("Ошибка: Невозможно преобразовать строку в число");
+            println!("Err to str");
         }
     }
 
     let mut summ:i64 = 0;
-    for item in &couples_num{
-        summ += item;
+    for itm in &couples_num{
+        summ += itm;
     }
     println!("{}", &summ);
-}
+    }
+
 
 
 
